7a9cbcb813f37e363670bcc784b2869607d4588f
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2020-03-18 20:38:25 +0200

    replaced scripter with bsvJS and wrote some of the logic for Private Key sweeping

--- bitUriParser.js
+++ bitUriParser.js
@@ -1,151 +1,7 @@
 var url = require("url");
 var http = require("http");
 var https = require("https");
-
-var scripter = (function() {
-  var base58Alphabet =
-    "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-
-  var to_b58 = function(
-    B, //Uint8Array raw byte input
-    A //Base58 characters (i.e. "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
-  ) {
-    var d = [],
-      s = "",
-      i,
-      j,
-      c,
-      n;
-    for (i in B) {
-      (j = 0), (c = B[i]);
-      s += c || s.length ^ i ? "" : A[0];
-      while (j in d || c) {
-        n = d[j];
-        n = n ? n * 256 + c : c;
-        c = (n / 58) | 0;
-        d[j] = n % 58;
-        j++;
-      }
-    }
-    while (j--) s += A[d[j]];
-    return s;
-  };
-
-  var from_b58 = function(
-    S, //Base58 string
-    A //Base58 characters (i.e. "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
-  ) {
-    var d = [],
-      b = [],
-      i,
-      j,
-      c,
-      n;
-    for (i in S) {
-      (j = 0), (c = A.indexOf(S[i]));
-      if (c < 0) return undefined;
-      c || b.length ^ i ? i : b.push(0);
-      while (j in d || c) {
-        n = d[j];
-        n = n ? n * 58 + c : c;
-        c = n >> 8;
-        d[j] = n % 256;
-        j++;
-      }
-    }
-    while (j--) b.push(d[j]);
-    return new Uint8Array(b);
-  };
-
-  function toHexString(byteArray) {
-    return Array.from(byteArray, function(byte) {
-      return ("0" + (byte & 0xff).toString(16)).slice(-2);
-    }).join("");
-  }
-
-  function hex2littleEndian(hexValue) {
-    var hexParts = [];
-    for (var i = 0; i < hexValue.length; i += 2)
-      hexParts.push(hexValue.substr(i, 2));
-    return hexParts.reverse().join("");
-  }
-
-  var _2bytesLimit = Math.pow(16, 4);
-  var _4bytesLimit = Math.pow(16, 8);
-
-  function hexValueInScript(hexString) {
-    if (hexString.length % 2 == 1) hexString = "0" + hexString;
-
-    var len = hexString.length / 2;
-
-    if (hexString === "00")
-      // OP_FALSE
-      return "00";
-
-    if (len < 76) return ("0" + len.toString(16)).slice(-2) + hexString;
-    else if (76 <= len && len < 256)
-      return "4c" + ("0" + len.toString(16)).slice(-2) + hexString;
-    else if (256 <= len && len < _2bytesLimit)
-      return (
-        "4d" +
-        hex2littleEndian(("000" + len.toString(16)).slice(-4)) +
-        hexString
-      );
-    else if (_2bytesLimit <= len && len < _4bytesLimit)
-      return (
-        "4e" +
-        hex2littleEndian(("0000000" + len.toString(16)).slice(-8)) +
-        hexString
-      );
-  }
-
-  function p2pkh(address) {
-    var pub = from_b58(address, base58Alphabet);
-    var pubCheckSum = pub.slice(21);
-    var pubMain = pub.slice(1, 21);
-    var pubHash160 = toHexString(pubMain);
-
-    // TODO: Check the checksum and throw exception if address is invalid
-
-    var resultScript = "76a9" + hexValueInScript(pubHash160) + "88ac";
-    return resultScript;
-  }
-
-  function str2hex(str) {
-    if (Array.isArray(str))
-      return str.map(function(part) {
-        return str2hex(part);
-      });
-
-    var result = "";
-    for (var i = 0; i < str.length; i++) {
-      var hex = str.charCodeAt(i).toString(16);
-      result += ("0" + hex).slice(-2);
-    }
-    return result;
-  }
-
-  function op_return(hexValues, use_op_false) {
-    if (use_op_false === undefined) use_op_false = true;
-
-    if (typeof hexValues == "string") hexValues = [hexValues];
-    if (!Array.isArray(hexValues))
-      throw new Error(
-        "op_return method expects an array of hexadecimal strings"
-      );
-    var resultScript = use_op_false ? "006a" : "6a";
-    for (var i = 0; i < hexValues.length; i++) {
-      resultScript = resultScript + hexValueInScript(hexValues[i]);
-    }
-    return resultScript;
-  }
-
-  return {
-    p2pkh: p2pkh,
-    op_return: op_return,
-    str2hex: str2hex
-  };
-})();
+var bsv = require("bsv");
 
 // the schemes are ordered - more strict to less strict
 var schemes = [
@@ -315,8 +171,23 @@ var schemes = [
 ];
 
 function create_PrivateKey_Inputs(uri, o) {
-  throw new Error("PrivateKey Not Implemented");
-  // TODO: Implement this method
+  var key = bsv.PrivateKey.fromString(uri.host);
+  var address = bsv.Address.fromPrivateKey(key);
+  var utxoData = await o.checkUtxosOfAddressFunction(address.toString());
+
+  var utxos = utxoData.map(i => {
+    return {
+      txid: i.txid.toString(),
+      vout: parseInt(i.vout.toString()),
+      satoshis: parseInt(i.satoshis.toString()),
+      scriptPubKey: i.scriptPubKey.toString(),
+      privkey: uri.host,
+      scriptType: findScriptType(i.scriptPubKey.toString()),
+    };
+  }).map(i=>{
+      i.scriptSig = generateScriptSigForUtxo(i);
+      return i;
+  });
 }
 async function create_Paymail_Output(uri, o) {
   return {
@@ -340,7 +211,8 @@ function create_BIP275_BIP282_Inputs(uri, o) {
       txid: i.txid,
       vout: parseInt(i.vout),
       satoshis: parseInt(i.value),
-      scriptSig: i.scriptSig
+      scriptSig: i.scriptSig,
+      privkey: undefined
     };
   });
 }
@@ -386,7 +258,7 @@ function create_BIP272_BIP282_Inputs(uri, o) {
 }
 function create_BIP21_Output(uri, o) {
   return {
-    script: scripter.p2pkh(uri.host),
+    script: bsv.Script.fromAddress(uri.host).toHex(),
     satoshis: parseInt(
       parseFloat(uri.searchParams["amount"]).toFixed(8) * 100000000
     )
@@ -396,6 +268,20 @@ function create_BIP72_Outputs(uri, o) {
   throw new Error("BIP72 Not Implemented");
   // TODO: Implement this method
   // TODO: Add MEMO property to the 'o' object
+}
+
+function findScriptType(scriptPubKey) {
+  // Return P2PK, P2PKH or null 
+
+  throw new Error("Not Implemented");
+  // TODO: Implement this method
+}
+
+function generateScriptSigForUtxo(params) {
+  // { txid, vout, satoshis, scriptPubKey, privkey, scriptType }
+
+  throw new Error("Not Implemented");
+  // TODO: Implement this method
 }
 
 function findUriType(bitcoinUri, options) {
@@ -465,9 +351,11 @@ function get(uri, o) {
     }).on("error", err => reject(err));
   });
 }
-async function checkUtxosOfPrivKey(privkey, o) {
-  throw new Error("Not Implemented");
+async function checkUtxosOfAddress(address) {
+  var url = "https://api.mattercloud.net/api/v3/main/address/" + address + "/utxo"
+  return JSON.parse(await get(uri));
 }
+
 async function resolvePaymail(paymail, o) {
   var paymailResolveUrl =
     "https://api.bitsent.net/paymail/" +
@@ -512,7 +400,7 @@ defaultOptions = {
   debugLog: () => {
     /** no logging by default */
   },
-  checkUtxosOfPrivKeyFunction: checkUtxosOfPrivKey,
+  checkUtxosOfAddressFunction: checkUtxosOfAddress,
   paymailResolverFunction: resolvePaymail
 };
 
